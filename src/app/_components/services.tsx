ddd207b9485dcc766a8b1b0e3b17b880736281ec
Arthur Silva <arthur.gbs.dev@gmail.com>
2026-02-17 13:09:15 -0300

    Finalizando o componente Testmonials

--- src/app/_components/services.tsx
+++ src/app/_components/services.tsx
@@ -63,7 +63,7 @@ export function Services() {
         <section className="bg-white py-16">
             <div className="container mx-auto px-4">
 
-                <h2 className="text-4xl font-bold mb-12">Nossos Serviços</h2>
+                <h2 className="text-4xl font-bold mb-12 text-center">Nossos Serviços</h2>
                  
                     <div className="relative">
 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import { About } from "./_components/about";
 import { Hero } from "./_components/hero";
 import { Services } from "./_components/services";
+import { Testmonials } from "./_components/testmonials";
 
 export default function Home() {
 
@@ -9,6 +10,7 @@ export default function Home() {
       <Hero />
       <About />
       <Services />
+      <Testmonials />
     </main>
   )
 }
